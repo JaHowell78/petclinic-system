90d5bc549e67632f9b9eafe965d48d50664c341a
Howell <jmhowell@a07817b4bf07.ant.amazon.com>
2022-02-03 20:41:13 -0500

    Trying again

--- src/main/java/org/springframework/samples/petclinic/system/ApplicationController.java
+++ src/main/java/org/springframework/samples/petclinic/system/ApplicationController.java
@@ -11,7 +11,7 @@ class ApplicationController {
 
     @RequestMapping("/")
     public String home() {
-        return "Welcome to PetClinic, Meow.";
+        return "Welcome to PetClinic";
     }
 
     private static final Logger logger = LoggerFactory.getLogger(ApplicationController.class);
