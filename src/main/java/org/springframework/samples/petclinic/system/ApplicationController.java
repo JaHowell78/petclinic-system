07150ef10a412d779d34c26ef606bbbb6a6e762c
Howell <jmhowell@a07817b4bf07.ant.amazon.com>
2022-02-03 22:52:32 -0500

    please

--- src/main/java/org/springframework/samples/petclinic/system/ApplicationController.java
+++ src/main/java/org/springframework/samples/petclinic/system/ApplicationController.java
@@ -3,10 +3,12 @@ package org.springframework.samples.petclinic.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SpringBootApplication
 class ApplicationController {
 
     @RequestMapping("/")
